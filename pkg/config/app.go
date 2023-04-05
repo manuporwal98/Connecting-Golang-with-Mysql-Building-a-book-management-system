e93e028dec8aee8d633be12dcc24431441f4a848
I535105 <manu.porwal@sap.com>
2023-04-05 11:21:11 +0530

    Updated the sql dB details and connected successfully with Mysql

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "manu:porwal@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:manu@tcp(localhost:3306)/golang_book_management?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
